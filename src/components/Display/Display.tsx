7c8283345fc2f7378ede11863a6845ed44b073f4
Joey Norkin <joey.norkin@gmail.com>
2023-06-27 21:02:32 -0400

    Improve styles

--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -10,8 +10,8 @@ export const Display = () => {
     <div className={styles.display}>
       {wallet.accounts.length > 0 &&
         <>
-          <div>Wallet Account: {wallet.accounts[0]}</div>
-          <div>Wallet Balance: {wallet.balance}</div>
+          <div>Addr: {wallet.accounts[0]}</div>
+          <div>Balance: {wallet.balance}</div>
           <div>Hex ChainId: {wallet.chainId}</div>
           <div>Numeric ChainId: {formatChainAsNum(wallet.chainId)}</div>
         </>
